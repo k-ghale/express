4e287c99fb8b7b9eea3dcfb4131843bf36ab9fca
Kabin <kabinghale1@gmail.com>
2024-05-26 01:16:52 +0545

    basic express app

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
+
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
-app.get('/',(req,res) => {
-    console.log('User hit the home page');
-    res.status(200).send('Home Page')
-})
+//setup static and middleware
+app.use(express.static('./public'))
 
-app.get('/about',(req,res) => {
-    res.status(200).send('About Page!')
-})
+//Simply dump them in public
+//SSR
+// app.get('/',(req,res) => {
+//     res.sendFile(path.resolve(__dirname,'./practice app/index.html'))
+// })
 
 app.all('*',(req,res) => {
-    res.status(404).send(`<h1>Resource not found!</h1>`)
+    res.status(404).send(`Resource not found!`)
 })
 
 app.listen(5000,() => {
-    console.log(`Server is listening on port 5000`);
+    console.log('Server is listening on port: 5000');
 })
