22db2d1cfc7fc314efe1b7a60cae88c9b576d606
Kabin <kabinghale1@gmail.com>
2024-05-26 23:56:12 +0545

    json and req-params

--- final/3-express-basics.js
+++ final/3-express-basics.js
@@ -18,3 +18,26 @@ app.all('*',(req,res) => {
 app.listen(5000,() => {
     console.log(`Server is listening on port 5000`);
 })
+
+
+// const express = require('express')
+// const path = require('path')
+
+// const app = express()
+
+//setup static and middleware
+// app.use(express.static('./public'))
+
+//Simply dump them in public
+//SSR
+// app.get('/',(req,res) => {
+//     res.sendFile(path.resolve(__dirname,'./practice app/index.html'))
+// })
+
+// app.all('*',(req,res) => {
+//     res.status(404).send(`Resource not found!`)
+// })
+
+// app.listen(5000,() => {
+//     console.log('Server is listening on port: 5000');
+// })
