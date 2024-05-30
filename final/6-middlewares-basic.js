4a1072278e821d04ded258c716ff167fb0042bba
Kabin <kabinghale1@gmail.com>
2024-05-30 21:57:48 +0545

    middleware basics

--- final/6-middlewares-basic.js
+++ final/6-middlewares-basic.js
@@ -1,9 +1,14 @@
-
 const express = require('express')
-const logger = require('./logger.js')
 
 const app = express()
 
+const logger = (req,res,next) =>{
+    const method = req.method
+    const url = req.url
+    const time = new Date().getFullYear()
+    console.log(method,url,time)
+    next()
+}
 
 app.get('/',logger,(req,res)=> {
    res.send('Home Page')
